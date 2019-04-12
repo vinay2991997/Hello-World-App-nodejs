3a9ac4e76d678368a6027bc2224dae45628cefdb
Vinay Garg <vinay.garg01@nagarro.com>
2019-04-12 12:55:26 +0530

    Update Port Number

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express()
 
+const PORT = process.env.PORT || 8989
+
 app.get('/',(req, res) => {
     res.send('Hello World')
 })
 
-app.listen(8989)
+app.listen(PORT)
